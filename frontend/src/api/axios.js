e1d617e84aac76053eea00ff8559d53c4efe4f6b
soukidv <soukainamouradi58@gmail.com>
2025-04-09 18:57:03 +0100

    Fix: downgrade React to v18 for react-beautiful-dnd compatibility

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: import.meta.env.VITE_API_BASE_URL, 
 });
-
 
 // Attach token to requests automatically
 API.interceptors.request.use((req) => {
